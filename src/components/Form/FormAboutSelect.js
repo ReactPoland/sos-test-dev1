bae2effc198bca97e29d58fe8be585ab2bfe2ce1
Tolik <talibasya@gmail.com>
2017-03-31 11:34:18 +0200

    style select

--- src/components/Form/FormAboutSelect.js
+++ src/components/Form/FormAboutSelect.js
@@ -2,18 +2,27 @@ import React, { PropTypes } from 'react'
 
 const FormSelect = ({ onChange, value, label }) => {
   const onChangeHandler = (ev) => onChange(ev.target.value)
+  const selectClickHandler = (ev) => {
+    ev.preventDefault()
+    console.log(ev.target)
+  }
 
   return (
     <div className='form-block-step2-item' >
       {label && (
         <div className='form-block-birth-label' style={{ color: label.color }} >{label.text}</div>
       )}
-      <select value={value} onChange={onChangeHandler} className='form-block-about-select'>
-        <option value='null' />
-        <option value='val1'>option1</option>
-        <option value='val2'>option2</option>
-        <option value='val3'>option3</option>
-      </select>
+      <div className='form-block-about-select-wrap' >
+        <i className='material-icons form-block-about-select-ic-arrow'
+          onClick={selectClickHandler} >keyboard_arrow_down</i>
+        <select value={value} onChange={onChangeHandler} className='form-block-about-select'>
+          <option value='null' />
+          <option value='val1'>option1</option>
+          <option value='val2'>option2</option>
+          <option value='val3'>option3</option>
+        </select>
+      </div>
+
     </div>
   )
 }
